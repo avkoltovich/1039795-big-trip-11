35b38a0ffcac611508217a436b85d10802c3f342
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-27 12:41:21 +0300

    refactor: переписыват функции stats по принципу DRY

--- src/components/header/menu.js
+++ src/components/header/menu.js
@@ -3,8 +3,8 @@ import AbstractComponent from '../abstract-component.js';
 const createMenuTemplate = () => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
-      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-      <a class="trip-tabs__btn" href="#">Stats</a>
+      <a id="table" class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a id="stats" class="trip-tabs__btn" href="#">Stats</a>
     </nav>`
   );
 };
@@ -13,8 +13,8 @@ export default class Menu extends AbstractComponent {
   constructor() {
     super();
 
-    this._tableButton = this._getMenuButtons()[0];
-    this._statsButton = this._getMenuButtons()[1];
+    this._tableButton = this.getElement().querySelector(`#table`);
+    this._statsButton = this.getElement().querySelector(`#stats`);
   }
 
   getTemplate() {
@@ -23,35 +23,31 @@ export default class Menu extends AbstractComponent {
 
   setTableButtonHandler(handler) {
     this._tableButton.addEventListener(`click`, (evt) => {
-        evt.preventDefault();
-        if (!this._tableButton.classList.contains(`trip-tabs__btn--active`)) {
-          this._tableButton.classList.toggle(`trip-tabs__btn--active`);
+      evt.preventDefault();
+      if (!this._tableButton.classList.contains(`trip-tabs__btn--active`)) {
+        this._tableButton.classList.toggle(`trip-tabs__btn--active`);
 
-          if (this._statsButton.classList.contains(`trip-tabs__btn--active`)) {
-            this._statsButton.classList.toggle(`trip-tabs__btn--active`);
-          }
-
-          handler();
+        if (this._statsButton.classList.contains(`trip-tabs__btn--active`)) {
+          this._statsButton.classList.toggle(`trip-tabs__btn--active`);
         }
-      });
+
+        handler();
+      }
+    });
   }
 
   setStatsButtonHandler(handler) {
     this._statsButton.addEventListener(`click`, (evt) => {
-        evt.preventDefault();
-        if (!this._statsButton.classList.contains(`trip-tabs__btn--active`)) {
-          this._statsButton.classList.toggle(`trip-tabs__btn--active`);
+      evt.preventDefault();
+      if (!this._statsButton.classList.contains(`trip-tabs__btn--active`)) {
+        this._statsButton.classList.toggle(`trip-tabs__btn--active`);
 
-          if (this._tableButton.classList.contains(`trip-tabs__btn--active`)) {
-            this._tableButton.classList.toggle(`trip-tabs__btn--active`);
-          }
-
-          handler();
+        if (this._tableButton.classList.contains(`trip-tabs__btn--active`)) {
+          this._tableButton.classList.toggle(`trip-tabs__btn--active`);
         }
-      });
-  }
 
-  _getMenuButtons() {
-    return this.getElement().querySelectorAll(`.trip-tabs__btn`);
+        handler();
+      }
+    });
   }
 }
--- src/components/stats/charts/time-spend-chart.js
+++ src/components/stats/charts/time-spend-chart.js
@@ -1,30 +1,17 @@
 import {BAR_HEIGHT, ChartValues, tripPointIconMap} from '../../../helpers/const.js';
 
-import Moment from 'moment';
 import Chart from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-export const timeSpendChart = (ctx, events) => {
-  const parseData = events
-    .reduce((time, event) => {
-      const dateFrom = new Moment(event.dateFrom);
-      const dateTo = new Moment(event.dateTo);
-      const diff = Moment.duration(dateTo.diff(dateFrom)).hours();
-
-      time[event.type] = (time[event.type] || 0) + diff;
-      return time;
-    }, {});
-
-  const sortedData = Object.entries(parseData).sort((a, b) => b[1] - a[1]);
-  const data = Object.fromEntries(sortedData);
-
-  ctx.height = BAR_HEIGHT * sortedData.length;
+export const timeSpendChart = (ctx, data) => {
+  const dataKeys = Object.keys(data);
+  ctx.height = BAR_HEIGHT * dataKeys.length;
 
   return new Chart(ctx, {
     plugins: [ChartDataLabels],
     type: `horizontalBar`,
     data: {
-      labels: Object.keys(data).map((label) => `${tripPointIconMap[label]} ${label.toUpperCase()}`),
+      labels: dataKeys.map((label) => `${tripPointIconMap[label]} ${label.toUpperCase()}`),
       datasets: [{
         data: Object.values(data),
         backgroundColor: `#ffffff`,
