9777336ed5c02121c9f1cbfb5ec6236c261dc11c
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-27 23:43:33 +0300

    feat: добавляет Модель EventAdapter

---
+++ src/models/event.js
@@ -0,0 +1,20 @@
+export default class EventAdapter {
+  constructor(data) {
+    this.basePrice = data[`base_price`];
+    this.dateFrom = new Date(data[`date_from`]);
+    this.dateTo = new Date(data[`date_to`]);
+    this.destination = data[`destination`];
+    this.id = data[`id`];
+    this.isFavorite = Boolean(data[`is_favorite`]);
+    this.offers = data[`offers`];
+    this.type = data[`type`];
+  }
+
+  static parseEvent(data) {
+    return new EventAdapter(data);
+  }
+
+  static parseEvents(data) {
+    return data.map(EventAdapter.parseEvent);
+  }
+}
