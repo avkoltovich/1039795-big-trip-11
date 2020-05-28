6f5a171a66b5daed7795582703cc33ecf3cf1898
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-28 21:30:31 +0300

    refactor: перерабатывает функцию получения данных с сервера

--- src/api.js
+++ src/api.js
@@ -1,6 +1,12 @@
 import EventAdapter from './models/event.js';
 
-const API = class {
+const ServerUrl = {
+  EVENTS: `https://11.ecmascript.pages.academy/big-trip/points`,
+  OFFERS: `https://11.ecmascript.pages.academy/big-trip/offers`,
+  DESTINATIONS: `https://11.ecmascript.pages.academy/big-trip/destinations`
+};
+
+export default class API {
   constructor(authorization) {
     this._authorization = authorization;
   }
@@ -21,30 +27,37 @@ const API = class {
       });
   }
 
-  _getEvents() {
-    const headers = new Headers();
-    headers.append(`Authorization`, this._authorization);
+  _checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  }
 
-    return fetch(`https://11.ecmascript.pages.academy/big-trip/points`, {headers})
+  _getEvents() {
+    return this._loadData({url: ServerUrl.EVENTS})
       .then((response) => response.json())
       .then(EventAdapter.parseEvents);
   }
 
   _getDestinations() {
-    const headers = new Headers();
-    headers.append(`Authorization`, this._authorization);
-
-    return fetch(`https://11.ecmascript.pages.academy/big-trip/destinations`, {headers})
+    return this._loadData({url: ServerUrl.DESTINATIONS})
       .then((response) => response.json());
   }
 
   _getOffers() {
-    const headers = new Headers();
-    headers.append(`Authorization`, this._authorization);
-
-    return fetch(`https://11.ecmascript.pages.academy/big-trip/offers `, {headers})
+    return this._loadData({url: ServerUrl.OFFERS})
       .then((response) => response.json());
   }
-};
 
-export default API;
+  _loadData({url, method = `GET`, body = null, headers = new Headers()}) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(url, {method, body, headers})
+      .then(this._checkStatus)
+      .catch((err) => {
+        throw err;
+      });
+  }
+}
