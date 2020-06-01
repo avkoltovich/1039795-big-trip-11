37c4c9906aaca7d4b2307e993ea5150d3bdf6b46
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-06-01 14:41:31 +0300

    fix: исправляет ошибки линтера

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import EventAdapter from '../models/event-adapter.js';
 
-const STATUS_REDIRECTION  = 300;
+const STATUS_REDIRECTION = 300;
 const STATUS_OK = 200;
 
 const ServerUrl = {
@@ -69,7 +69,7 @@ export default class API {
   }
 
   _checkStatus(response) {
-    if (response.status >= STATUS_OK && response.status <= STATUS_REDIRECTION) {
+    if (response.status >= STATUS_OK && response.status < STATUS_REDIRECTION) {
       return response;
     } else {
       throw new Error(`${response.status}: ${response.statusText}`);
